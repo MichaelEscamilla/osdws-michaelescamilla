181c8d58d8ef31f431e76e281495920615c3ef22
Michael Escamilla <MichaelEscamilla@protonmail.com>
2025-05-05 13:38:39 -0500

    Refactor: Update language removal script to include additional languages
    - Expanded the list of languages to remove from the BootMedia in `Remove_Language_Folders-Non_en-us.ps1`.
    - Updated the script to remove non en-us language artifacts, enhancing the cleanup process for the WinPE environment.
    - Improved logging message to reflect the removal of multiple languages instead of just 'en-gb'.
    
    This change ensures a more comprehensive removal of unnecessary language folders, optimizing the BootMedia for better performance.

--- winpe-mediascript/LanguageFolders/Remove_Language_Folders-Non_en-us.ps1
+++ winpe-mediascript/LanguageFolders/Remove_Language_Folders-Non_en-us.ps1
@@ -1,12 +1,43 @@
-# Removes the en-gb language folder from the BootMedia
-
-# Language to Remove
+# Languages to Remove - Modified Today
 $Languages = @(
+    'bg-bg'
+    'cs-cz'
+    'da-dk'
+    'de-de'
+    'el-gr'
     'en-gb'
+    'es-es'
+    'es-mx'
+    'et-ee'
+    'fi-fi'
+    'fr-ca'
+    'fr-fr'
+    'hr-hr'
+    'hu-hu'
+    'it-it'
+    'ja-jp'
+    'ko-kr'
+    'lt-lt'
+    'lv-lv'
+    'nb-no'
+    'nl-nl'
+    'pl-pl'
+    'pt-br'
+    'pt-pt'
+    'ro-ro'
+    'ru-ru'
+    'sk-sk'
+    'sl-si'
+    'sr-latn-rs'
+    'sv-se'
+    'tr-tr'
+    'uk-ua'
+    'zh-cn'
+    'zh-tw'
 )
 
 Start-Transcript
-Write-Host -ForegroundColor DarkGray "[$((Get-Date).ToString('HH:mm:ss'))] Removing en-gb language artifacts from BootMedia"
+Write-Host -ForegroundColor DarkGray "[$(Get-Date -format G)] Removing non en-us language artifacts from BootMedia"
 foreach ($Language in $Languages) {
     if (Test-Path "$MediaPath\$Language") {
         Remove-Item -Path "$MediaPath\$Language" -Recurse
@@ -19,6 +50,20 @@ foreach ($Language in $Languages) {
     }
     if (Test-Path "$MediaPath\EFI\Microsoft\Boot\$Language") {
         Remove-Item -Path "$MediaPath\EFI\Microsoft\Boot\$Language" -Recurse
+    }
+
+    
+    if (Test-Path "$MediaPathEX\$Language") {
+        Remove-Item -Path "$MediaPathEX\$Language" -Recurse
+    }
+    if (Test-Path "$MediaPathEX\Boot\$Language") {
+        Remove-Item -Path "$MediaPathEX\Boot\$Language" -Recurse
+    }
+    if (Test-Path "$MediaPathEX\EFI\Boot\$Language") {
+        Remove-Item -Path "$MediaPathEX\EFI\Boot\$Language" -Recurse
+    }
+    if (Test-Path "$MediaPathEX\EFI\Microsoft\Boot\$Language") {
+        Remove-Item -Path "$MediaPathEX\EFI\Microsoft\Boot\$Language" -Recurse
     }
 }
 Stop-Transcript
