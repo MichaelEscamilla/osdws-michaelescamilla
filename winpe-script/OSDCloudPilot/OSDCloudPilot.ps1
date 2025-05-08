5271471fb30f3ca96121fb53a7c30d26cad203fa
Michael Escamilla <MichaelEscamilla@protonmail.com>
2025-05-08 09:50:38 -0500

    Refactor: Simplify PowerShell command usage in OSDCloudPilot script
    - Updated PowerShell command syntax to ensure consistent use of 'powershell.exe' across the script.
    - Removed redundant comments and streamlined the notes section for clarity.
    - Adjusted the title in the startnet.cmd content for better context.
    - Improved logging format for better readability.
    
    These changes enhance the maintainability and readability of the script, ensuring a smoother workflow in the WinPE environment.

--- winpe-script/OSDCloudPilot/OSDCloudPilot.ps1
+++ winpe-script/OSDCloudPilot/OSDCloudPilot.ps1
@@ -4,75 +4,33 @@
 <#
     .NOTES
     The initial PowerShell commands should always contain the -WindowStyle Hidden parameter to prevent the PowerShell window from appearing on the screen.
-	powershell, -WindowStyle Hidden -Command ...
+	powershell.exe -WindowStyle Hidden -Command {command}
 
 	This will prevent PowerShell from rebooting since the window will not be visible.
-	powershell, -WindowStyle Hidden -NoExit
+	powershell.exe -WindowStyle Hidden -NoExit -Command {command}
 
 	The final PowerShell command should contain the -NoExit parameter to keep the PowerShell window open and to prevent the WinPE environment from restarting.
-	powershell, -NoExit -WindowStyle Hidden -Command ...
-
-	Launch the On Screen Keyboard
-	powershell, -WindowStyle Hidden -Command Invoke-WpeinitPSCommand Show-WinpeStartOSK -Run Asynchronous -WindowStyle Hidden
-	[-Run Asynchronous] This command will run and continue to the next Winpeshl.ini command, without waiting for this command to finish.
-	[-WindowStyle Hidden] The -WindowStyle Hidden hides the PowerShell window that launches the On Screen Keyboard.
-
-	Hardware Devices with OK Status
-	powershell, -WindowStyle Hidden -Command Invoke-WpeinitPSCommand Show-WinpeStartWindowHardware -Run Asynchronous -WindowStyle Minimized -NoExit
-	[-Run Asynchronous] This command will run and continue to the next Winpeshl.ini command, without waiting for this command to finish.
-	[-WindowStyle Minimized] The PowerShell window will open minimized, allowing the user to view if necessary.
-	[-NoExit] The PowerShell window will persist until closed by the user.
-
-	Hardware Devices without OK Status
-	powershell, -WindowStyle Hidden -Command Invoke-WpeinitPSCommand Show-WinpeStartWindowHardwareErrors -Run Asynchronous -WindowStyle Maximized -NoExit
-	[-Run Asynchronous] This command will run and continue to the next Winpeshl.ini command, without waiting for this command to finish.
-	[-WindowStyle Maximized] The PowerShell window will open maximized to ensure visiblity of the Hardware information.
-	[-NoExit] The PowerShell window will persist until closed by the user.
-
-	Wireless Connectivity
-	powershell, -WindowStyle Hidden -Command Invoke-WpeinitPSCommand Show-WinpeStartWindowWiFi -Run Synchronous
-	[-Run Synchronous] Default. This command will run and wait for this command to finish before continuing to the next Winpeshl.ini command.
-
-	Update OSD PowerShell
-	powershell, -WindowStyle Hidden -Command Invoke-WpeinitUpdateModule -Name OSD
-	[-Name OSD] Name of the PowerShell Module to update
-	[-Run Synchronous] Default. This command will run and wait for this command to finish before continuing to the next Winpeshl.ini command.
-	[-WindowStyle Normal] Default.
-
-	powershell, -WindowStyle Hidden -NoExit -Command Invoke-WpeinitPSCommand Show-WinpeStartWindowDeviceInfo -NoExit
-	[-Run Synchronous] Default. This command will run and wait for this command to finish before continuing to the next Winpeshl.ini command.
-	[-NoExit] The PowerShell window will persist until closed by the user.
-
-	Using the Winpeshl.ini file to launch the PowerShell script that will initialize the OSDCloud environment.
-
-	Startup-OSD.xml is RunSynchronous with two entries, which means they run in order and the first must complete before the second starts.
-	If the first entry must exit successfully (Exit 0) for the second entry to run.
-	This means if you close it before it is finished, the second entry will not start.
-	This Unattend is complete when all entries have finished.
-
-	Startup-OSD2.xml is RunAsynchronous with two entries, which means they run at the same time.
-	Since they are asynchronous, they will run regardless of the exit code of the previous entry.
-	These entries will not keep WinPE from restarting automatically, so it doesn't matter if there is a -NoExit or -Wait parameter.
-	This Unattend is complete when all entries have started.
+	powershell.exe -WindowStyle Hidden -NoExit -Command {command}
 
 	Wpeinit and Startnet.cmd: Using WinPE Startup Scripts
 	https://learn.microsoft.com/en-us/windows-hardware/manufacture/desktop/wpeinit-and-startnetcmd-using-winpe-startup-scripts?view=windows-11
 #>
 #=================================================
-#region Copy PowerShell Modules
+# Copy PowerShell Modules
+# Make sure they are up to date on your device before running this script.
 $ModuleNames = @('OSD', 'OSDCloud')
 $ModuleNames | ForEach-Object {
 	$ModuleName = $_
-	Write-Host -ForegroundColor DarkGray "[$((Get-Date).ToString('HH:mm:ss'))][$($MyInvocation.MyCommand.Source)] Copy PowerShell Module to BootImage: $ModuleName"
-	# Add a step to install the latest
+	Write-Host -ForegroundColor DarkGray "[$(Get-Date -format G)] [$($MyInvocation.MyCommand.Source)] Copy PowerShell Module to BootImage: $ModuleName"
 	Copy-PSModuleToWindowsImage -Name $ModuleName -Path $MountPath | Out-Null
+	# As an alternative, you can use the following command to get the latest from PowerShell Gallery:
+	# Save-Module -Name $ModuleName -Path "$MountPath\Program Files\WindowsPowerShell\Modules" -Force
 }
-#endregion
 #=================================================
-#region Startnet.cmd
+# Startnet.cmd
 $Content = @'
 @echo off
-title OSDCloud Workflow WinPE Startup
+title OSDCloud Pilot WinPE Startup
 wpeinit
 wpeutil DisableFirewall
 wpeutil UpdateBootInfo
@@ -86,11 +44,6 @@ powershell.exe -w h -c Invoke-OSDCloudPEStartup Info
 wpeutil Reboot
 pause
 '@
-
-Write-Host -ForegroundColor DarkGray "[$((Get-Date).ToString('HH:mm:ss'))][$($MyInvocation.MyCommand.Source)] Adding $MountPath\Windows\System32\startnet.cmd"
+Write-Host -ForegroundColor DarkGray "[$(Get-Date -format G)] [$($MyInvocation.MyCommand.Source)] Adding $MountPath\Windows\System32\startnet.cmd"
 $Content | Out-File -FilePath "$MountPath\Windows\System32\startnet.cmd" -Encoding ascii -Width 2000 -Force
-#endregion
 #=================================================
-<#
-
-#>
